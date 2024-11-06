30cde184722c56dae0fe9fca79bde313f383e006
anushka3107-bit <bhardwajanushka180@gmail.com>
2024-11-06 21:04:37 +0530

    all about query-limti and search

--- index.js
+++ index.js
@@ -10,21 +10,46 @@ app.get('/',(req,res)=>{
     )
 })
 
-app.get('/api/phone/:phoneID', (req,res) => {
-    // params
-    console.log(req.params)
-    const {phoneID} = req.params;
-    const singleProduct = phone.find((product) =>
-        product.id === Number(phoneID)
-    )
+// app.get('/api/phone/:phoneID', (req,res) => {
+//     // params
+//     console.log(req.params)
+//     const {phoneID} = req.params;
+//     const singleProduct = phone.find((product) =>
+//         product.id === Number(phoneID)
+//     )
 
-    if(!singleProduct){
-        res.status(404).send('product not found')
+//     if(!singleProduct){
+//         res.status(404).send('product not found')
+//     }
+//     res.json(singleProduct);
+// })
+
+
+// query
+app.get('/api/v1/query', (req,res)=>{
+    let sortedData = [...phone];
+    console.log(sortedData)
+    const {search,limit} = req.query;
+    
+    if(search){
+        sortedData = sortedData.filter((data)=>{
+            return(
+                data.name
+                .toLowerCase()
+                .startsWith(search)
+            )
+        })
+        // console.log(sortedData)
     }
-    res.json(singleProduct);
+
+    if(limit){
+        sortedData = sortedData.slice(
+            1,Number(limit)
+        )
+    }
+
+    res.json(sortedData)
 })
-
-
 
 app.listen(3000,()=>{
     console.log('hello server is created ....');
