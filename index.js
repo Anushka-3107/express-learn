84904867b3e4044f62432f0ca6ff4f20de8b3996
anushka3107-bit <bhardwajanushka180@gmail.com>
2024-11-07 11:32:32 +0530

    middleware

--- index.js
+++ index.js
@@ -1,56 +1,13 @@
-
 const express = require('express');
 const app = express();
-const phone = require('./data.js')
 
+const logger = (req,res,next) =>{
+    console.log(`${new Date()}, Request[${req.method}, [${req.url}]]`);
+    next();
+}
 
-app.get('/',(req,res)=>{
-    res.send(
-        '<h1>Home Page</h1><a href="/api/phone">Phone</a>'
-    )
-})
-
-// app.get('/api/phone/:phoneID', (req,res) => {
-//     // params
-//     console.log(req.params)
-//     const {phoneID} = req.params;
-//     const singleProduct = phone.find((product) =>
-//         product.id === Number(phoneID)
-//     )
-
-//     if(!singleProduct){
-//         res.status(404).send('product not found')
-//     }
-//     res.json(singleProduct);
-// })
-
-
-// query
-app.get('/api/v1/query', (req,res)=>{
-    let sortedData = [...phone];
-    console.log(sortedData)
-    const {search,limit} = req.query;
-    
-    if(search){
-        sortedData = sortedData.filter((data)=>{
-            return(
-                data.name
-                .toLowerCase()
-                .startsWith(search)
-            )
-        })
-        // console.log(sortedData)
-    }
-
-    if(limit){
-        sortedData = sortedData.slice(
-            1,Number(limit)
-        )
-    }
-
-    res.json(sortedData)
-})
+app.use(logger);
 
 app.listen(3000,()=>{
-    console.log('hello server is created ....');
-});
+    console.log('hello, server started..')
+})
