a90093e05a9fecce7f48c9f9da0f3dafc10efbcf
anushka3107-bit <bhardwajanushka180@gmail.com>
2024-11-09 23:56:52 +0530

    learning http methods

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
-
-// const logger = (req,res,next) =>{
-//     console.log(`${new Date()}, Request[${req.method}], [${req.url}]`);
-//     next();
-// }
 
 
-// app.use(logger);
-
-app.use(morgan('dev'))
-
-app.get('/',(req,res)=>{
-    res.send('Home')
-})
-
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
-
-
-app.get('/contact',(req,res)=>{
-    res.send('contact')
-})
 
 
 app.listen(3000,()=>{
     console.log('hello, server started..')
 })
+
